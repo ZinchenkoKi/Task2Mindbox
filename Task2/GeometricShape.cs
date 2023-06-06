7b70a9e6d37d682cac0067f80890ebacd4fb74c5
Kirill <67413320+ZinchenkoKi@users.noreply.github.com>
2023-06-06 12:39:49 +0300

    06.06.2023

---
+++ Task2/GeometricShape.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task2
+{
+    public class GeometricShape
+    {
+        public double Area(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius), "The value cannot be less than one.");
+            }
+            else
+            {
+                double area = Math.PI * Math.Pow(radius, 2);
+                return RoundResult(area, 6);
+            }
+        }
+
+        public double Area(double first, double second, double third)
+        {
+            if (!IsExist(first, second, third))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                double halfMeter = (first + second + third) / 2;
+                double area = Math.Sqrt(halfMeter * (halfMeter - first) * (halfMeter - second) * (halfMeter - third));
+                return RoundResult(area, 6);
+            } 
+        }
+
+        public bool IsRectangular(double first, double second, double third)
+        {
+            if (!IsExist(first, second, third))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return Math.Pow(first,2) + Math.Pow(second, 2) == Math.Pow(third, 2);
+            }
+        }
+
+        private double RoundResult(double value, int roundingValue)
+        {
+            return Math.Round(value, roundingValue);
+        }
+
+        private bool IsExist(double a, double b, double c)
+        {
+           return a + b > c && b + c > a && c + a > b && a > 0 && b > 0 && c > 0;
+        }
+    }
+}
---
+++ Task2/GeometricShapeTest.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Task2
+{
+    [TestFixture]
+    public class GeometricShapeTest
+    {
+        [Test]
+        public void AreaCircleRadiusDozens()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(6361.725124, shape.Area(45));
+        }
+
+        [Test]
+        public void AreaCircleRadiusHundreds()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(172021.04734, shape.Area(234));
+        }
+
+        [Test]
+        public void AreaCircleRadiusThousands()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(31415.926536, shape.Area(100));
+        }
+
+        [Test]
+        public void AreaCircleRadiusEqualZero()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.Throws<ArgumentOutOfRangeException>(() => shape.Area(0));
+        }
+
+        [Test]
+        public void AreaCircleRadiusNegative()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.Throws<ArgumentOutOfRangeException>(() => shape.Area(-1));
+        }
+
+        [Test]
+        public void TriangleExist()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.Throws<ArgumentException> (() => shape.Area(1,6,8));
+        }
+
+        [Test]
+        public void TriangleNegativeValue()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.Throws<ArgumentException>(() => shape.Area(-6, 6, 8));
+        }
+
+        [Test]
+        public void AreaTriangleRadiusDozens()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(1788.854382, shape.Area(60,60,80));
+        }
+
+        [Test]
+        public void AreaTriangleRadiusHundreds()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(13586.983771, shape.Area(123,243,222));
+        }
+
+        [Test]
+        public void AreaTriangleRadiusThousands()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(732688.036286, shape.Area(1432, 1276, 1223));
+        }
+
+        [Test]
+        public void TriangleRectangular()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(true, shape.IsRectangular(3, 4, 5));
+        }
+
+        [Test]
+        public void TriangleNotRectangular()
+        {
+            GeometricShape shape = new GeometricShape();
+            Assert.AreEqual(false, shape.IsRectangular(6, 6, 8));
+        }
+    }
+}
